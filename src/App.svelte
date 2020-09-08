eec776497052956e79c9da69e1d78732810de217
Ian Seng <ianseng@outlook.com>
2020-09-08 17:09:11 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	let name = 'world';
+	let age = 30;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<h1>Hello {name}!, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
